3b9f6440cb5c142926b506522043c72261687a2c
divyamittal123 <121234250+divyamittal123@users.noreply.github.com>
2022-12-22 18:37:44 +0530

    Update Assignmen1.py

---
+++ Assignmen1.py
@@ -0,0 +1,45 @@
+a=int(input("Enter the value of a : "))
+b=int(input("Enter the value of b : "))
+c=int(input("Enter the value of c : "))
+
+d=(a+b+c)/3
+print("The average of a,b and c is %f" % d)
+
+
+rate=20/100
+std_ddc=10000
+dpd=int(input("Enter the number of dependents : "))
+Gross_income=int(input("Enter the gross income : "))
+tax_inc=Gross_income-std_ddc-(dpd*3000)
+
+Tax=tax_inc*rate
+
+print("\nThe tax rate is 20%")
+
+print("\nThe taxable income is : %d \nThe tax is : %d " % (tax_inc,Tax))
+
+seconds=int(input("Enter the nua=25"))
+b=int('25')
+c=int(25.0)
+
+sum=a+b+c
+str(sum)
+
+seconds=int(input("Enter the number of seconds : "))
+minutes=seconds//60
+sec=seconds%60
+print("%d minutes and %d seconds" % (minutes,sec))
+
+import math
+print("The values of sines and cosines from 0" + chr(176) + " to " + "345" + chr(176))
+angle = 0
+for angle in range(0, 360, 15):
+    valuesin = round(math.sin(math.radians(angle)), 4)
+    valuecos = round(math.cos(math.radians(angle)), 4)
+    print("sin " + str(angle) + chr(176) + " = " + str(valuesin) + "       " + "cos " + str(angle) + chr(176) + " = " + str(valuecos))
+
+sec=seconds%60
+print("%d minutes and %d seconds" % (minutes,sec))
+
+
+
